cd2fbf43537f9e0b969e9c936df9842a16605adb
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2023-01-17 13:11:48 -0600

    Added snap scroll using react-use-scroll-snap library

--- src/Components/SnapContent/SnapContent.tsx
+++ src/Components/SnapContent/SnapContent.tsx
@@ -1,12 +1,17 @@
 import './SnapContent.css';
 import { useBlink } from '../../Hooks/useBlink';
 import { AboutMe } from '../AboutMe';
+import { useRef } from 'react';
+import useScrollSnap from 'react-use-scroll-snap';
 
 const SnapContent: React.FC = () => {
     const [isLit] = useBlink();
-        
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    useScrollSnap({ ref: scrollRef, duration: 0.5, delay: 0.5 });
+    
     return (
-        <div className='snap-main-container'>
+        <div className='snap-main-container' ref={scrollRef}>
             <AboutMe isLit={isLit}/>
         </div>
     )
