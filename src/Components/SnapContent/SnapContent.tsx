065983c8c594c7c5e2c006bb564864f6d11118de
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2023-01-24 19:35:44 -0600

    Base project section

--- src/Components/SnapContent/SnapContent.tsx
+++ src/Components/SnapContent/SnapContent.tsx
@@ -2,6 +2,7 @@ import './SnapContent.css';
 import { AboutMeSection } from '../AboutMeSection';
 import { TechStackSection } from '../TechStackSection';
 import { ContactSection } from '../ContactSection';
+import { ProjectsSection } from '../ProjectsSection';
 
 
 const SnapContent: React.FC = () => {
@@ -9,6 +10,7 @@ const SnapContent: React.FC = () => {
     return <main className='snap-main-container'>
         <AboutMeSection />
         <TechStackSection/>
+        <ProjectsSection />
         <ContactSection />
     </main>
 }
