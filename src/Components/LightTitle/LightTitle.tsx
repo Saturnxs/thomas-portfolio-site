7eba049c1f492b36ba73f321153415bca0f49944
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2023-01-26 19:52:15 -0600

    Reformatted components

--- src/Components/LightTitle/LightTitle.tsx
+++ src/Components/LightTitle/LightTitle.tsx
@@ -8,20 +8,18 @@ const LightTitle: React.FC<ILightTitle> = ({ title, icon, text }) => {
 
     const [isLit, toggleIsLit] = useBlink();
 
-    return (
-        <header className="light-title-container">
-            <VisibilitySensor
-                onChange={(isVisible: boolean) => { toggleIsLit() }}
-                scrollDelay={10}
-            >
-                <div className='light-title'>
-                    <img src={icon} alt={title + " icon"} className={isLit ? 'light-on title-icon' : ' title-icon'}></img>
-                    <h1 className={isLit ? 'title light-on' : 'title'}>{title}</h1>
-                </div>
-            </VisibilitySensor>
-            {text}
-        </header>
-    );
+    return <header className="light-title-container">
+        <VisibilitySensor
+            onChange={(isVisible: boolean) => { toggleIsLit() }}
+            scrollDelay={10}
+        >
+            <div className='light-title'>
+                <img src={icon} alt={title + " icon"} className={isLit ? 'light-on title-icon' : ' title-icon'}></img>
+                <h1 className={isLit ? 'title light-on' : 'title'}>{title}</h1>
+            </div>
+        </VisibilitySensor>
+        {text}
+    </header>
 };
 
 export { LightTitle };
--- src/Components/ProjectsSection/ProjectsSection.tsx
+++ src/Components/ProjectsSection/ProjectsSection.tsx
@@ -2,15 +2,13 @@ import { LightTitle } from '../LightTitle';
 import './ProjectsSection.css';
 
 const ProjectsSection: React.FC = () => {
-    return <>
-        <section className="projects-section" id="projects">
-            <LightTitle
-                title="projects."
-                icon='static/svg/mars-icon.svg'
-                text={<p className='body-text'>Some <strong>code</strong> and <strong>design/production</strong> projects in which I've had worked.</p>}
-            />
-        </section>
-    </>
+    return <section className="projects-section" id="projects">
+        <LightTitle
+            title="projects."
+            icon='static/svg/mars-icon.svg'
+            text={<p className='body-text'>Some <strong>code</strong> and <strong>design/production</strong> projects in which I've had worked.</p>}
+        />
+    </section>
 };
 
 export { ProjectsSection };
